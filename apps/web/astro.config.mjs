02a2cffd8d0726ec56acb95ba145e278792cf935
Aquitano / Thomas Breindl <Aquitano@Protonmail.com>
2023-05-04 20:30:56 +0200

    feat(web): add new scroll down animation

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,14 +1,22 @@
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import critters from "astro-critters";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [
+    sitemap(),
     tailwind(),
+    critters(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    compress({
+      img: false,
     }),
   ],
   vite: {
