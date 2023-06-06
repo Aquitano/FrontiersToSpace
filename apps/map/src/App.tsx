17bd458517a3f48a7cd8eb58c687a99817d0a188
Aquitano / Thomas Breindl <Aquitano@Protonmail.com>
2023-06-06 10:45:07 +0200

    chore(deps): fix dependency

--- apps/map/src/App.tsx
+++ apps/map/src/App.tsx
@@ -8,149 +8,149 @@ import { repeaters } from './repeaters.js';
 import { storageLayer } from './storageLayer.js';
 
 const App = () => {
-  const { position } = useMap();
-  const [map, setMap] = useState(null);
-  const [progress, setProgress] = useState(0);
-  const [total, setTotal] = useState(0);
+	const { position } = useMap();
+	const [map, setMap] = useState(null);
+	const [progress, setProgress] = useState(0);
+	const [total, setTotal] = useState(0);
 
-  useEffect(() => {
-    if (map) {
-      const tileLayerOffline = L.tileLayer.offline(
-        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-          subdomains: 'abc',
-          minZoom: 8,
-          maxZoom: 10,
-        },
-      );
+	useEffect(() => {
+		if (map) {
+			const tileLayerOffline = L.tileLayer.offline(
+				'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+				{
+					attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+					subdomains: 'abc',
+					minZoom: 8,
+					maxZoom: 10,
+				},
+			);
 
-      tileLayerOffline.addTo(map);
+			tileLayerOffline.addTo(map);
 
-      const controlSaveTiles = L.control.savetiles(tileLayerOffline, {
-        zoomlevels: [8, 9, 10],
-        confirm(
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          layer: { _tilesforSave: string | any[] },
-          succescallback: () => void,
-        ) {
-          if (window.confirm(`Save ${layer._tilesforSave.length}`)) {
-            succescallback();
-          }
-        },
-        confirmRemoval(successCallback: () => void) {
-          if (window.confirm('Remove all the tiles?')) {
-            successCallback();
-          }
-        },
-        saveText: '<i class="fas fa-download" aria-hidden="true" title="Save tiles"></i>',
-        rmText: '<i class="fas fa-trash" aria-hidden="true"  title="Remove tiles"></i>',
-      });
+			const controlSaveTiles = L.control.savetiles(tileLayerOffline, {
+				zoomlevels: [8, 9, 10],
+				confirm(
+					// eslint-disable-next-line @typescript-eslint/no-explicit-any
+					layer: { _tilesforSave: string | any[] },
+					succescallback: () => void,
+				) {
+					if (window.confirm(`Save ${layer._tilesforSave.length}`)) {
+						succescallback();
+					}
+				},
+				confirmRemoval(successCallback: () => void) {
+					if (window.confirm('Remove all the tiles?')) {
+						successCallback();
+					}
+				},
+				saveText: '<i class="fas fa-download" aria-hidden="true" title="Save tiles"></i>',
+				rmText: '<i class="fas fa-trash" aria-hidden="true"  title="Remove tiles"></i>',
+			});
 
-      controlSaveTiles.addTo(map);
+			controlSaveTiles.addTo(map);
 
-      const layerswitcher = new Control.Layers(
-        {
-          'osm (offline)': tileLayerOffline,
-        },
-        null,
-        { collapsed: false },
-      ).addTo(map);
-      // add storage overlay
-      storageLayer(tileLayerOffline, layerswitcher);
+			const layerswitcher = new Control.Layers(
+				{
+					'osm (offline)': tileLayerOffline,
+				},
+				null,
+				{ collapsed: false },
+			).addTo(map);
+			// add storage overlay
+			storageLayer(tileLayerOffline, layerswitcher);
 
-      let locProgress: number;
-      tileLayerOffline.on(
-        'savestart',
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (e: { _tilesforSave: string | any[] }) => {
-          locProgress = 0;
-          setTotal(e._tilesforSave.length);
-        },
-      );
-      tileLayerOffline.on('savetileend', () => {
-        locProgress += 1;
-        setProgress(locProgress);
-      });
-    }
-  }, [map]);
+			let locProgress: number;
+			tileLayerOffline.on(
+				'savestart',
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				(e: { _tilesforSave: string | any[] }) => {
+					locProgress = 0;
+					setTotal(e._tilesforSave.length);
+				},
+			);
+			tileLayerOffline.on('savetileend', () => {
+				locProgress += 1;
+				setProgress(locProgress);
+			});
+		}
+	}, [map]);
 
-  return (
-    <>
-      <p
-        style={{
-          zIndex: 100,
-          position: 'fixed',
-          top: 'auto',
-          bottom: 0,
-          left: 0,
-          right: 'auto',
-          color: 'red',
-          backgroundColor: 'white',
-        }}
-      >
-        Downloaded: {progress} of {total}
-      </p>
-      <MapContainer
-        // @ts-expect-error
-        center={position}
-        zoom={8}
-        scrollWheelZoom={true}
-        ref={setMap}
-        style={{ minHeight: '100vh', minWidth: '100vw', zIndex: 0 }}
-      >
-        <Marker position={position}>
-          <Popup>Eigener Standort</Popup>
-        </Marker>
-        {repeaters.map((marker) => (
-          <Marker position={[marker[0].lat, marker[0].lng]}>
-            <Popup>
-              {marker.map((element, index) => (
-                // if index === 0 then detais are open
-                // else details are closed
-                <details open={index === 0}>
-                  <summary>{element.name}</summary>
+	return (
+		<>
+			<p
+				style={{
+					zIndex: 100,
+					position: 'fixed',
+					top: 'auto',
+					bottom: 0,
+					left: 0,
+					right: 'auto',
+					color: 'red',
+					backgroundColor: 'white',
+				}}
+			>
+				Downloaded: {progress} of {total}
+			</p>
+			<MapContainer
+				// @ts-expect-error
+				center={position}
+				zoom={8}
+				scrollWheelZoom={true}
+				ref={setMap}
+				style={{ minHeight: '100vh', minWidth: '100vw', zIndex: 0 }}
+			>
+				<Marker position={position}>
+					<Popup>Eigener Standort</Popup>
+				</Marker>
+				{repeaters.map((marker) => (
+					<Marker position={[marker[0].lat, marker[0].lng]}>
+						<Popup>
+							{marker.map((element, index) => (
+								// if index === 0 then detais are open
+								// else details are closed
+								<details open={index === 0}>
+									<summary>{element.name}</summary>
 
-                  <table>
-                    <tbody>
-                      <tr>
-                        <td>Call</td>
-                        <td>{element.name}</td>
-                      </tr>
-                      <tr>
-                        <td>Position</td>
-                        <td>{element.qth}</td>
-                      </tr>
-                      <tr>
-                        <td>Ausgabe / Eingabe</td>
-                        <td>
-                          {element.output} / {element.input}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Offset</td>
-                        <td>
-                          {Number.parseFloat(element.output) - Number.parseFloat(element.input)}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Mode</td>
-                        <td>{element.mode}</td>
-                      </tr>
-                      <tr>
-                        <td>Bemerkung</td>
-                        <td>{element.description}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </details>
-              ))}
-            </Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-    </>
-  );
+									<table>
+										<tbody>
+											<tr>
+												<td>Call</td>
+												<td>{element.name}</td>
+											</tr>
+											<tr>
+												<td>Position</td>
+												<td>{element.qth}</td>
+											</tr>
+											<tr>
+												<td>Ausgabe / Eingabe</td>
+												<td>
+													{element.output} / {element.input}
+												</td>
+											</tr>
+											<tr>
+												<td>Offset</td>
+												<td>
+													{Number.parseFloat(element.output) - Number.parseFloat(element.input)}
+												</td>
+											</tr>
+											<tr>
+												<td>Mode</td>
+												<td>{element.mode}</td>
+											</tr>
+											<tr>
+												<td>Bemerkung</td>
+												<td>{element.description}</td>
+											</tr>
+										</tbody>
+									</table>
+								</details>
+							))}
+						</Popup>
+					</Marker>
+				))}
+			</MapContainer>
+		</>
+	);
 };
 
 export default App;
--- apps/map/vite.config.ts
+++ apps/map/vite.config.ts
@@ -4,37 +4,37 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeManifestIcons: true,
-      manifest: {
-        name: 'Offline Repeater Map',
-        short_name: 'Repeater Map',
-        description: 'Repeater map for offline use',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/vite.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml',
-          },
-          {
-            src: '/vite.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml',
-          },
-        ],
-      },
-      devOptions: {
-        enabled: true,
-      },
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeManifestIcons: true,
+			manifest: {
+				name: 'Offline Repeater Map',
+				short_name: 'Repeater Map',
+				description: 'Repeater map for offline use',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: '/vite.svg',
+						sizes: '192x192',
+						type: 'image/svg+xml',
+					},
+					{
+						src: '/vite.svg',
+						sizes: '512x512',
+						type: 'image/svg+xml',
+					},
+				],
+			},
+			devOptions: {
+				enabled: true,
+			},
 
-      // Offline
-      workbox: {
-        globPatterns: ['**/*'],
-      },
-    }),
-  ],
+			// Offline
+			workbox: {
+				globPatterns: ['**/*'],
+			},
+		}),
+	],
 });
