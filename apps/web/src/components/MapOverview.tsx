89c4879edb27531bceabb476136a5b22224c4a79
Aquitano / Thomas Breindl <Aquitano@Protonmail.com>
2023-07-28 23:37:28 +0200

    feat(web): add crosshair & clickable polyline

--- apps/web/src/components/MapOverview.tsx
+++ apps/web/src/components/MapOverview.tsx
@@ -9,11 +9,13 @@ import {
 	Tooltip,
 } from 'chart.js';
 import annotationPlugin from 'chartjs-plugin-annotation';
-import L from 'leaflet';
+import { CrosshairPlugin } from 'chartjs-plugin-crosshair';
+import L, { LatLngTuple } from 'leaflet';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
+import 'leaflet-geometryutil';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet/dist/leaflet.css';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { MapContainer, Marker, Polyline, Popup, TileLayer } from 'react-leaflet';
 import markerIcon from '../assets/balloon.png';
@@ -33,12 +35,14 @@ ChartJS.register(
 	Tooltip,
 	Legend,
 	annotationPlugin,
+	CrosshairPlugin,
 );
 
 const optionsTL = {
 	responsive: true,
 	plugins: {
 		tooltip: {
+			mode: 'index',
 			callbacks: {
 				label: function (context: { dataset: { label: string }; parsed: { y: any } }) {
 					const label = context.dataset.label || '';
@@ -51,6 +55,7 @@ const optionsTL = {
 					return `${label}: ${value}`;
 				},
 			},
+			intersect: false,
 		},
 		legend: {
 			labels: {
@@ -69,9 +74,27 @@ const optionsTL = {
 						backgroundColor: 'rgba(155, 99, 32, 0.3)',
 						content: 'Ballon geplatzt',
 						display: true,
-						yAdjust: -75,
+						yAdjust: -40,
 					},
 				},
+			},
+		},
+		crosshair: {
+			line: {
+				color: '#F66', // crosshair line color
+				width: 1, // crosshair line width
+			},
+			sync: {
+				enabled: true, // enable trace line syncing with other charts
+				group: 2, // chart group
+				suppressTooltips: false, // suppress tooltips when showing a synced tracer
+			},
+			zoom: {
+				enabled: true, // enable zooming
+				zoomboxBackgroundColor: 'rgba(66,133,244,0.2)', // background color of zoom box
+				zoomboxBorderColor: '#48F', // border color of zoom box
+				zoomButtonText: 'Reset Zoom', // reset zoom button text
+				zoomButtonClass: 'reset-zoom', // reset zoom button class
 			},
 		},
 	},
@@ -110,6 +133,7 @@ const optionsPA = {
 	responsive: true,
 	plugins: {
 		tooltip: {
+			mode: 'index',
 			callbacks: {
 				label: function (context: { dataset: { label: string }; parsed: { y: any } }) {
 					const label = context.dataset.label || '';
@@ -122,6 +146,7 @@ const optionsPA = {
 					return `${label}: ${value}`;
 				},
 			},
+			intersect: false,
 		},
 		legend: {
 			labels: {
@@ -140,9 +165,27 @@ const optionsPA = {
 						backgroundColor: 'rgba(155, 99, 32, 0.3)',
 						content: 'Ballon geplatzt',
 						display: true,
-						xAdjust: -30,
+						yAdjust: -40,
 					},
 				},
+			},
+		},
+		crosshair: {
+			line: {
+				color: '#F66', // crosshair line color
+				width: 1, // crosshair line width
+			},
+			sync: {
+				enabled: true, // enable trace line syncing with other charts
+				group: 1, // chart group
+				suppressTooltips: false, // suppress tooltips when showing a synced tracer
+			},
+			zoom: {
+				enabled: true, // enable zooming
+				zoomboxBackgroundColor: 'rgba(66,133,244,0.2)', // background color of zoom box
+				zoomboxBorderColor: '#48F', // border color of zoom box
+				zoomButtonText: 'Reset Zoom', // reset zoom button text
+				zoomButtonClass: 'reset-zoom', // reset zoom button class
 			},
 		},
 	},
@@ -192,22 +235,41 @@ type ChartData = {
 	}[];
 };
 
-const MapMain = () => {
-	const [isTLFullScreen, setIsTLFullScreen] = useState(false);
-	const [isPAFullScreen, setIsPAFullScreen] = useState(false);
+const initialState = {
+	isTLFullScreen: false,
+	isPAFullScreen: false,
 
 	// Chart data for temperature and humidity
-	const [chartDataTL, setChartDataTL] = useState<ChartData | {}>({});
-	const [showChartTL, setShowChartTL] = useState(false);
+	chartDataTL: {},
+	showChartTL: false,
 
 	// Chart data for pressure and altitude
-	const [chartDataPA, setChartDataPA] = useState<ChartData | {}>({});
-	const [showChartPA, setShowChartPA] = useState(false);
+	chartDataPA: {},
+	showChartPA: false,
+	location: [0, 0],
+};
 
-	const [location, setLocation] = useState<[number, number]>([0, 0]);
+const MapMain = () => {
+	const [state, setState] = useState(initialState);
+
 	const map = useRef<L.Map>(null);
+	const polyline = useRef<L.Polyline>(null);
 
 	const [data] = useState<StratosphereData[]>(inputData);
+
+	const toggleTLFullScreen = useCallback(() => {
+		setState((prevState) => ({
+			...prevState,
+			isTLFullScreen: !prevState.isTLFullScreen,
+		}));
+	}, []);
+
+	const togglePAFullScreen = useCallback(() => {
+		setState((prevState) => ({
+			...prevState,
+			isPAFullScreen: !prevState.isPAFullScreen,
+		}));
+	}, []);
 
 	/**
 	 * Effect to update chart data when weather data changes.
@@ -216,37 +278,43 @@ const MapMain = () => {
 		if (data) {
 			const times = data.map((entry) => {
 				const date = new Date(entry.date * 1000);
+				if (date.getMinutes().toString().length === 1) {
+					return `${date.getHours()}:0${date.getMinutes()}`;
+				}
 				return `${date.getHours()}:${date.getMinutes()}`;
 			});
 			const temps = data.map((entry) => entry.tmp.toFixed(2));
 			const humidity = data.map((entry) => entry.hum);
 
-			setChartDataTL({
-				labels: times,
-				datasets: [
-					{
-						label: 'Temperatur (°C)',
-						data: temps,
-						fill: false,
-						backgroundColor: 'rgb(75, 192, 192)',
-						borderColor: 'rgba(75, 192, 192, 0.5)',
-						tension: 1,
-						pointRadius: 0.4,
-						yAxisID: 'temp',
-					},
-					{
-						label: 'Luftfeuchtigkeit (%)',
-						data: humidity,
-						fill: false,
-						backgroundColor: 'rgb(255, 99, 132)',
-						borderColor: 'rgba(255, 99, 132, 0.5)',
-						tension: 1,
-						pointRadius: 0.4,
-						yAxisID: 'hum',
-					},
-				],
-			});
-			setShowChartTL(true);
+			setState((prevState) => ({
+				...prevState,
+				chartDataTL: {
+					labels: times,
+					datasets: [
+						{
+							label: 'Temperatur (°C)',
+							data: temps,
+							fill: false,
+							backgroundColor: 'rgb(75, 192, 192)',
+							borderColor: 'rgba(75, 192, 192, 0.6)',
+							tension: 1,
+							pointRadius: 0.4,
+							yAxisID: 'temp',
+						},
+						{
+							label: 'Luftfeuchtigkeit (%)',
+							data: humidity,
+							fill: false,
+							backgroundColor: 'rgb(255, 99, 132)',
+							borderColor: 'rgba(255, 99, 132, 0.6)',
+							tension: 1,
+							pointRadius: 0.4,
+							yAxisID: 'hum',
+						},
+					],
+				},
+				showChartTL: true,
+			}));
 		}
 	}, [data]);
 
@@ -265,6 +333,9 @@ const MapMain = () => {
 
 			const times = pressureData.map((entry) => {
 				const date = new Date(entry.time * 1000);
+				if (date.getMinutes().toString().length === 1) {
+					return `${date.getHours()}:0${date.getMinutes()}`;
+				}
 				return `${date.getHours()}:${date.getMinutes()}`;
 			});
 
@@ -272,32 +343,35 @@ const MapMain = () => {
 			const altitudes = pressureData.map((entry) => entry.altitude.toFixed(2));
 
 			if (pressures.length > 0 && altitudes.length > 0) {
-				setChartDataPA({
-					labels: times,
-					datasets: [
-						{
-							label: 'Luftdruck (hPa)',
-							data: pressures,
-							yAxisID: 'pressure',
-							fill: false,
-							backgroundColor: 'rgb(75, 192, 192)',
-							borderColor: 'rgba(75, 192, 192, 0.5)',
-							tension: 1,
-							pointRadius: 0.4,
-						},
-						{
-							label: 'Höhe (m)',
-							data: altitudes,
-							yAxisID: 'altitude',
-							fill: false,
-							backgroundColor: 'rgb(255, 99, 132)',
-							borderColor: 'rgba(255, 99, 132, 0.5)',
-							tension: 1,
-							pointRadius: 0.4,
-						},
-					],
-				});
-				setShowChartPA(true);
+				setState((prevState) => ({
+					...prevState,
+					chartDataPA: {
+						labels: times,
+						datasets: [
+							{
+								label: 'Luftdruck (hPa)',
+								data: pressures,
+								yAxisID: 'pressure',
+								fill: false,
+								backgroundColor: 'rgb(75, 192, 192)',
+								borderColor: 'rgba(75, 192, 192, 0.6)',
+								tension: 1,
+								pointRadius: 0.4,
+							},
+							{
+								label: 'Höhe (m)',
+								data: altitudes,
+								yAxisID: 'altitude',
+								fill: false,
+								backgroundColor: 'rgb(255, 99, 132)',
+								borderColor: 'rgba(255, 99, 132, 0.6)',
+								tension: 1,
+								pointRadius: 0.4,
+							},
+						],
+					},
+					showChartPA: true,
+				}));
 			}
 		}
 	}, [data]);
@@ -308,22 +382,65 @@ const MapMain = () => {
 	useEffect(() => {
 		if (data) {
 			const lastDataPoint = data[data.length - 1];
-			setLocation([lastDataPoint.lat, lastDataPoint.lon]);
+			setState((prevState) => ({
+				...prevState,
+				location: [lastDataPoint.lat, lastDataPoint.lon],
+			}));
 		}
 	}, [data]);
 
 	const locations = data.map((entry) => [entry.lat, entry.lon]);
 	if (map.current) map.current.setView([48.56099, 13.44782], 10);
 
+	const handlePolylineClick = (event: { latlng: any }) => {
+		if (!map.current) return;
+
+		const threshold = 5;
+		const clickPoint = event.latlng;
+
+		for (let i = 0; i < locations.length - 1; i++) {
+			const start = locations[i] as LatLngTuple;
+			const end = locations[i + 1] as LatLngTuple;
+			const distance = L.GeometryUtil.distanceSegment(map.current, clickPoint, start, end);
+
+			if (distance < threshold) {
+				// Retrieve the corresponding data point
+				const entry = data[i];
+
+				// Format the data for the popup
+				const time = new Date(entry.date * 1000).toLocaleTimeString('de-DE', {
+					hour: '2-digit',
+					minute: '2-digit',
+				});
+				const content = `
+                Uhrzeit: ${time}<br>
+                Ort: ${entry.lat.toFixed(4)}, ${entry.lon.toFixed(4)}<br>
+                Temperatur: ${entry.tmp.toFixed(2)} °C<br>
+                Luftfeuchtigkeit: ${entry.hum} %<br>
+                Höhe: ${entry.alt.toFixed(2)} m<br>
+                Luftdruck: ${entry.pss.toFixed(2)} hPa
+            `;
+
+				// Create a new popup at the clicked location with the formatted data
+				L.popup().setLatLng([data[i].lat, data[i].lon]).setContent(content).openOn(map.current);
+
+				break;
+			}
+		}
+	};
+
 	return (
 		<div>
-			{showChartTL && (
+			{state.showChartTL && (
 				<div
-					className={`absolute left-0 top-0 ${
-						isTLFullScreen ? 'z-50 h-full w-full' : 'z-10 w-full md:w-1/3'
+					className={`absolute left-0 top-0 mr-10 overflow-hidden ${
+						state.isTLFullScreen ? 'z-50 h-full w-full' : 'z-10 w-full md:w-1/3'
 					} bg-slate-900`}
 				>
-					<button onClick={() => setIsTLFullScreen(!isTLFullScreen)} className="flex items-center">
+					<button
+						onClick={toggleTLFullScreen}
+						className="absolute left-0 top-0 flex items-center text-white"
+					>
 						<img
 							src={fullscreenIcon.src}
 							width={20}
@@ -331,20 +448,22 @@ const MapMain = () => {
 							alt="Toggle Full Screen"
 							className="invert"
 						/>
-						{/* @ts-ignore */}
-						{isTLFullScreen ? 'Verkleinern' : 'Vergrößern'}
+						{state.isTLFullScreen ? 'Verkleinern' : 'Vergrößern'}
 					</button>
 					{/* @ts-ignore */}
-					<Line data={chartDataTL} options={optionsTL} />
+					<Line data={state.chartDataTL} options={optionsTL} />
 				</div>
 			)}
-			{showChartPA && (
+			{state.showChartPA && (
 				<div
-					className={`absolute right-0 top-0 ${
-						isPAFullScreen ? 'z-50 h-full w-full' : 'z-10 w-full md:w-1/3'
+					className={`absolute right-0 top-0 ml-10 overflow-hidden ${
+						state.isPAFullScreen ? 'z-50 h-full w-full' : 'z-10 w-full md:w-1/3'
 					} bg-slate-900`}
 				>
-					<button onClick={() => setIsPAFullScreen(!isPAFullScreen)} className="flex items-center">
+					<button
+						onClick={togglePAFullScreen}
+						className="absolute left-0 top-0 flex items-center text-white"
+					>
 						<img
 							src={fullscreenIcon.src}
 							width={20}
@@ -352,14 +471,14 @@ const MapMain = () => {
 							alt="Toggle Full Screen"
 							className="invert"
 						/>
-						{isPAFullScreen ? 'Verkleinern' : 'Vergrößern'}
+						{state.isPAFullScreen ? 'Verkleinern' : 'Vergrößern'}
 					</button>
 					{/* @ts-ignore */}
-					<Line data={chartDataPA} options={optionsPA} />
+					<Line data={state.chartDataPA} options={optionsPA} />
 				</div>
 			)}
 			<MapContainer
-				center={location}
+				center={state.location as LatLngTuple}
 				zoom={13}
 				scrollWheelZoom={true}
 				style={{ height: '100vh', zIndex: 0 }}
@@ -369,10 +488,24 @@ const MapMain = () => {
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
-				{/* @ts-expect-error */}
-				{locations && <Polyline positions={locations} />}
+				{locations && (
+					<div>
+						<Polyline
+							positions={locations.map((location) => [location[0], location[1]] as LatLngTuple)}
+							ref={polyline}
+						/>
+						<Polyline
+							positions={locations.map((location) => [location[0], location[1]] as LatLngTuple)}
+							eventHandlers={{
+								click: handlePolylineClick,
+							}}
+							weight={25}
+							opacity={0}
+						/>
+					</div>
+				)}
 				<Marker
-					position={location}
+					position={state.location as LatLngTuple}
 					icon={L.icon({
 						iconUrl: markerIcon.src,
 						shadowUrl: markerShadow.src,
@@ -383,7 +516,8 @@ const MapMain = () => {
 					})}
 				>
 					<Popup>
-						Derzeitige Position: <br /> {location[0].toFixed(3)}, {location[1].toFixed(3)}
+						Ballon ist gelandet: <br /> {state.location[0].toFixed(3)},{' '}
+						{state.location[1].toFixed(3)} <br /> 15:09
 					</Popup>
 				</Marker>
 				<Marker
@@ -396,7 +530,7 @@ const MapMain = () => {
 					})}
 				>
 					<Popup>
-						Ballon ist geplatzt: <br /> 48.6063067, 12.9076828 <br /> 14:20
+						Ballon ist geplatzt: <br /> 48.606, 12.907 <br /> 14:20
 					</Popup>
 				</Marker>
 			</MapContainer>
